1cf5388f4b7a6577792c97fe09f9e06b1bc87e0a
sebashack <sebastian0092@gmail.com>
2023-05-10 22:15:55 -0500

    Introduce pings

--- hsAutoscaling/src/AutoScalingGroup/CRUD.hs
+++ hsAutoscaling/src/AutoScalingGroup/CRUD.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-module AutoScalingGroup.CRUD where
+module AutoScalingGroup.CRUD (
+    closeConn,
+    deleteInstance,
+    insertInstance,
+    insertMetric,
+    insertPing,
+) where
 
 import Data.Text (Text)
-import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Time.Clock (getCurrentTime)
 import Database.SQLite.Simple (
     Connection,
     Only (..),
@@ -15,15 +21,15 @@ import Database.SQLite.Simple (
  )
 import Text.RawString.QQ
 
-insertInstance :: Connection -> Text -> Text -> IO ()
-insertInstance conn insId privateId = do
+insertInstance :: Connection -> Text -> Text -> Text -> IO ()
+insertInstance conn insId privateIp privateDNSName = do
     now <- getCurrentTime
-    execute conn q (insId, privateId, now)
+    execute conn q (insId, privateIp, privateDNSName, now)
   where
     q =
         [r|
-        INSERT INTO instance (id, private_ip, created_at)
-        VALUES (?, ?, ?)
+        INSERT INTO instance (id, private_ip, private_dns_name, created_at)
+        VALUES (?, ?, ?, ?)
         |]
 
 insertMetric :: Connection -> Text -> Float -> Float -> IO ()
@@ -36,3 +42,20 @@ insertMetric conn insId cpuLoad httpLoad = do
         INSERT INTO metric (instance_id, cpu_load_percentage, http_load_percentage, created_at)
         VALUES (?, ?, ?, ?)
         |]
+
+insertPing :: Connection -> Text -> IO ()
+insertPing conn insId = do
+    now <- getCurrentTime
+    execute conn q (insId, now)
+  where
+    q =
+        [r|
+        INSERT INTO ping (instance_id, created_at)
+        VALUES (?, ?)
+        |]
+
+deleteInstance :: Connection -> Text -> IO ()
+deleteInstance conn insId = execute conn "DELETE FROM instance WHERE id=?" (Only insId)
+
+closeConn :: Connection -> IO ()
+closeConn = close
