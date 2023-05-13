f33b0f1713f2a1ece38793c4035b78001422f8fe
sebashack <sebastian0092@gmail.com>
2023-05-13 00:48:07 -0500

    Add delay

--- hsMonitor/app/Main.hs
+++ hsMonitor/app/Main.hs
@@ -2,11 +2,15 @@
 
 module Main (main) where
 
+import Control.Concurrent (threadDelay)
 import Control.Monad (forever, replicateM_)
 
 import Env (Env (..), mkEnv)
 import Grpc.Client (pushMetrics, runActions)
 import MetricGen (genRandomVar, genSeed, sampleLoad)
+
+tenSecs :: Int
+tenSecs = 10000000
 
 main :: IO ()
 main = do
@@ -15,3 +19,4 @@ main = do
         cpuLoad <- sampleLoad (seed env) (rvar env)
         httpLoad <- sampleLoad (seed env) (rvar env)
         (pushMetrics cpuLoad httpLoad undefined 5 client)
+        threadDelay tenSecs
