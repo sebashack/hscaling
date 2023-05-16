7ea400af4b06d748db5259013933b3681755065f
sebashack <sebastian0092@gmail.com>
2023-05-15 22:05:37 -0500

    Scale up or down based on global average

--- hsAutoscaling/src/AutoScalingGroup/Scaling.hs
+++ hsAutoscaling/src/AutoScalingGroup/Scaling.hs
@@ -5,6 +5,8 @@ module AutoScalingGroup.Scaling (scaleAction, initializeInstances) where
 import Control.Monad (replicateM_)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (asks)
+import Data.Maybe (listToMaybe)
+import Data.Text as T (pack)
 
 import AutoScalingGroup.AWS (runInstance, terminateInstance)
 import AutoScalingGroup.AWS as INF (InstanceInfo (..))
@@ -33,10 +35,22 @@ scaleAction :: ASGActionE ()
 scaleAction = do
     conn <- asks dbConn
     metricss <- liftIO $ selectInstanceMetrics conn
-    mapM_ scaleUpOrDown metricss
+    let f i (a1, a2, n) = (IM.cpuLoadPercentage i + a1, IM.httpLoadPercentage i + a2, n + 1)
+        (sumCpuLoad, sumHttpLoad, k) = foldr f (0, 0, 0 :: Float) metricss
+        avgCpuLoad = sumCpuLoad / k
+        avgHttpLoad = sumHttpLoad / k
+    logText
+        ( ">>>>>>> scale: average cpu = "
+            <> showT avgCpuLoad
+            <> ", average http = "
+            <> showT avgHttpLoad
+        )
+    scaleUpOrDown (listToMaybe metricss) avgCpuLoad avgHttpLoad
+  where
+    showT = T.pack . show
 
-scaleUpOrDown :: InstanceMetrics -> ASGActionE ()
-scaleUpOrDown m = do
+scaleUpOrDown :: Maybe InstanceMetrics -> Float -> Float -> ASGActionE ()
+scaleUpOrDown maybeIns avgCpuLoad avgHttpLoad = do
     maybeMaxCpuLoad <- asks appCpuMaxLoadPercentage
     maybeMaxHttpLoad <- asks appHttpMaxLoadPercentage
     minCount <- asks appMinInstances
@@ -49,30 +63,33 @@ scaleUpOrDown m = do
         (True, False) -> do
             info <- runInstance
             liftIO $ insertInstance conn (INF.instanceId info) (INF.privateIp info) (INF.privateDNSName info)
-            logText "scale: scaling up ..."
+            logText ">>>>>>> scale: scaling up ..."
         (False, True) -> do
-            terminateInstance $ IM.instanceId m
-            liftIO $ deleteInstance conn (IM.instanceId m)
-            logText "scale: scaling down ..."
+            case maybeIns of
+                Just m -> do
+                    terminateInstance $ IM.instanceId m
+                    liftIO $ deleteInstance conn (IM.instanceId m)
+                    logText ">>>>>>> scale: scaling down ..."
+                Nothing -> return ()
         (False, False) -> return ()
-        (True, True) -> logText "scale-WARNING: both scale-up and scale-down conditions are True"
+        (True, True) -> logText ">>>>>>> scale-WARNING: both scale-up and scale-down conditions are True"
   where
     shouldScaleUp :: Int -> Int -> Maybe Float -> Maybe Float -> Bool
     shouldScaleUp currentCount maxCount maybeMaxCpuLoad maybeMaxHttpLoad =
         if currentCount < maxCount
             then case (maybeMaxCpuLoad, maybeMaxHttpLoad) of
-                (Nothing, Nothing) -> cpuLoadPercentage m >= 60
-                (Nothing, Just httpLoad) -> httpLoadPercentage m >= httpLoad
-                (Just cpuLoad, Nothing) -> cpuLoadPercentage m >= cpuLoad
-                (Just cpuLoad, Just httpLoad) -> httpLoadPercentage m >= httpLoad || cpuLoadPercentage m >= cpuLoad
+                (Nothing, Nothing) -> avgCpuLoad >= 60
+                (Nothing, Just httpLoad) -> avgHttpLoad >= httpLoad
+                (Just cpuLoad, Nothing) -> avgCpuLoad >= cpuLoad
+                (Just cpuLoad, Just httpLoad) -> avgHttpLoad >= httpLoad || avgCpuLoad >= cpuLoad
             else False
     --
     shouldScaleDown :: Int -> Int -> Maybe Float -> Maybe Float -> Bool
     shouldScaleDown currentCount minCount maybeMaxCpuLoad maybeMaxHttpLoad =
         if currentCount > minCount
             then case (maybeMaxCpuLoad, maybeMaxHttpLoad) of
-                (Nothing, Nothing) -> cpuLoadPercentage m < 60
-                (Nothing, Just httpLoad) -> httpLoadPercentage m < httpLoad
-                (Just cpuLoad, Nothing) -> cpuLoadPercentage m < cpuLoad
-                (Just cpuLoad, Just httpLoad) -> httpLoadPercentage m < httpLoad && cpuLoadPercentage m < cpuLoad
+                (Nothing, Nothing) -> avgCpuLoad < 60
+                (Nothing, Just httpLoad) -> avgHttpLoad < httpLoad
+                (Just cpuLoad, Nothing) -> avgCpuLoad < cpuLoad
+                (Just cpuLoad, Just httpLoad) -> avgHttpLoad < httpLoad && avgCpuLoad < cpuLoad
             else False
